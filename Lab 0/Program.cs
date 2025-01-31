378474ac2d4643721db4d8fc2e09a6ad8fb76110
Leomathew25 <166417184+Leomathew25@users.noreply.github.com>
2025-01-30 20:32:06 -0700

    Update Program.cs

--- Lab 0/Program.cs
+++ Lab 0/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace Lab_0
+namespace Lab_0
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Task 1
+            Console.WriteLine("Enter a low number:");
+            int lowNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a high number: ");
+            int highNumber = int.Parse(Console.ReadLine());
+
+            int difference = lowNumber - highNumber;
+
+            Console.WriteLine($"The difference between {highNumber} and {lowNumber} is {difference}");
+            // Task 2
+            do
+            {
+                Console.WriteLine("Enter a positive low number: ");
+                lowNumber = int.Parse(Console.ReadLine());
+            }
+            while (lowNumber <= 0);
+            Console.WriteLine($"You entered a positive low number: {lowNumber}");
+
+            do
+            {
+                Console.WriteLine("Enter a high number greater than the low number: ");
+                highNumber = int.Parse(Console.ReadLine());
+            } while (highNumber <= lowNumber);
             
+            Console.WriteLine($"You entered a high number greater than the low number: {highNumber}.");
+            //Task 3
+            int[] numbers = new int[highNumber - lowNumber + 1];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = lowNumber + i;
+            }
+
+            Console.WriteLine("Numbers in reverse order");
+
         }
     }
 }
